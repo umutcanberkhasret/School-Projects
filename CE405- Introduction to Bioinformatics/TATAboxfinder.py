281a7ba0f60dff0567a39d7e0d4079d426c183be
Umutcan Berk Hasret <38487394+umutcanberkhasret@users.noreply.github.com>
2018-04-21 01:30:02 +0300

    Add files via upload

---
+++ CE405- Introduction to Bioinformatics/TATAboxfinder.py
@@ -0,0 +1,57 @@
+
+'''Simple algorithm to find "TATA" boxes that points where a genetic sequence can be
+read and decoded. '''
+
+fileContent = open("C:\Python27\ATgene.fa", "r")
+
+# While operating on files; "r" : read only , "w" : write , "a" : append
+# In FASTA  format, the line which contains > sign referred for description of the following gene.
+
+
+seq= ""
+
+for line in fileContent:
+	#To avoid description line, use smth.starstwith(">")
+	
+	if line.startswith(">"):
+		continue
+		
+	seq= seq+line[:-1]
+
+print seq , "\n"
+
+for ind,nuc in enumerate(seq):
+	if seq[ind:ind+7]=="TATAAAT":
+		print ind+1, "-> ",ind+7
+		print seq[ind: ind+7]
+		
+	if seq[ind:ind+7]== "TATAAAA" : 
+		print ind+1 , "->" , ind+7
+		print seq[ind: ind+7]
+		
+	if seq[ind:ind+7]=="TATATAA":
+		print ind+1, "-> ",ind+7
+		print seq[ind: ind+7]
+		
+	if seq[ind:ind+7]=="TATATAT":
+		print ind+1, "-> ",ind+7
+		print seq[ind: ind+7]
+
+		
+''' Same result could have been achieved with following simple code as well;
+
+bt1=seq.find("TATAAAT")
+bt2=seq.find("TATAAAA")  
+bt3=seq.find("TATATAA")
+bt4=seq.find("TATATAT")
+
+if bt1 != -1:
+ print bt1 
+if bt2 != -1:
+ print bt2  
+if bt3 != -1:
+ print bt3
+if bt4 != -1:
+ print bt4 
+ '''
+ 
---
+++ CE405- Introduction to Bioinformatics/UserInputOrientedGeneComplementery.py
@@ -0,0 +1,107 @@
+#Program converts the given DNA strand to its complementary DNA or RNA strand depending on the choice.
+
+def RNAcomplementaryconverter(DNA):
+	
+	rna_comp= ''
+	#we are creating a list since we can't operate on string indice by indice.
+	templist= list(DNA)
+	j=0
+	for i in templist:
+		
+		if templist[j] == 'A':
+			templist[j]= 'U'
+		
+		elif templist[j] == 'G':
+			templist[j]= 'C'
+		
+		elif templist[j] == 'T':
+			templist[j]= 'A'
+		
+		elif templist[j] == 'C':
+			templist[j]= 'G'
+		j=j+1
+	
+	
+	#at the beginning we have changed the type from string to list.
+	#by doing this procedure we have string again.
+	
+	rna_comp= ''.join(c for c in templist)
+	
+	#obtained strand must be reversed in order to have 3' to 5' relationship
+	#since we are creating its complementary.
+	rna_comp= ''.join(reversed(rna_comp))
+	
+	return rna_comp
+
+	
+	
+def DNAcomplementaryconverter(DNA):
+	
+	dna_comp= ''
+	#we are creating a list since we can't operate on string indice by indice.
+	templist= list(DNA)
+	j=0
+	for i in templist:
+		
+		if templist[j] == 'A':
+			templist[j]= 'T'
+		
+		elif templist[j] == 'G':
+			templist[j]= 'C'
+		
+		elif templist[j] == 'T':
+			templist[j]= 'A'
+		
+		elif templist[j] == 'C':
+			templist[j]= 'G'
+		j=j+1
+	
+	
+	#we changed the strand from string to list before
+	#by doing this procedure we have string again.
+	
+	dna_comp= ''.join(c for c in templist)
+	
+	#obtained strand must be reversed in order to have 3' to 5' relationship
+	#since we are creating its complementary.
+	dna_comp= ''.join(reversed(dna_comp))
+	
+	return dna_comp
+
+
+#MAIN PART...
+print " *****All strand that we will deal here, will be assumed as they start from 5' to 3' *******\n"
+		
+while 1:
+	
+	DNA_seq= ' '
+	#Getting the DNA strand that will be handled.
+	DNA_seq= raw_input('Please specify the DNA strand;\n')
+
+
+	print 'Specify the complementary strand type: (1 for DNA, 2 for RNA, 3 for exit...) ' 
+	strandtype= input()
+	print "\n\n\n"
+	
+	if strandtype==1:
+
+		DNA_comp= DNAcomplementaryconverter(DNA_seq)
+		print "Given DNA Strand: ", DNA_seq ,"\n"
+		print "It's complementary DNA strand: ", DNA_comp , "\n"
+		print "\n\n\n"
+
+	elif strandtype==2:
+
+		RNA_comp= RNAcomplementaryconverter(DNA_seq)
+		print "Given DNA Strand: ", DNA_seq , "\n"
+		print "It's complementary RNA strand: ", RNA_comp , "\n"
+		print "\n\n\n"
+	
+	elif input("Press 5 if you wish to continue or press 3 to exit...")==5 :
+		continue
+	else:
+		break 
+		
+		
+		
+		
